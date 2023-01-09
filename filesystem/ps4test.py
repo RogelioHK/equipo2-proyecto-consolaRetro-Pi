bae6284c364675ea51974d7c8b8aa3146f314bd6
Hernández Ku Rogelio <rogherku@comunidad.unam.mx>
2023-01-09 09:34:20 -0600

    file test

--- filesystem/ps4test.py
+++ filesystem/ps4test.py
@@ -4,11 +4,26 @@ class control(Controller):
 	def __init__(self, **kwargs):
 		Controller.__init__(self, **kwargs)
 
-	def on_options_press(self):
-		print("x")
+#	def on_options_press(self):
+#		print("x")
 
-	def on_R2_press(self, value):
-		print("")
+#	def on_R2_press(self, value):
+#		pass
+
+#	def on_R2_release(self):
+#		pass
+
+#	def on_L2_press(self, value):
+#		pass
+
+#	def on_L2_release(self):
+#		pass
+
+def konami_callback():
+	print("a")
+
+def ms():
+	return [{"inputs": ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'share', 'options'], "callback": konami_callback}]
 
 c0 = control(interface="/dev/input/js0", connecting_using_ds4drv=False)
-c0.listen(timeout=60)
+c0.listen(timeout=60, on_sequence=ms())
