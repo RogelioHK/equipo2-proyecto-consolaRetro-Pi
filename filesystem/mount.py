04726d27bd65085f94b44cf026e9d0f31b92b491
Hernández Ku Rogelio <rogherku@comunidad.unam.mx>
2023-01-12 03:01:14 -0600

    Update mount.py
    Añade un if, el cual busca si la carpeta "ROMS" existe en el almacenamiento externo. De ser así, continúa con su funcionamiento previo, caso contrario retorna un valor manipulado por emu.py, el cual indica que la carpeta buscada no existe

--- filesystem/mount.py
+++ filesystem/mount.py
@@ -1,10 +1,9 @@
 import os
 import time
 
-#print("Mounts", os.system("blkid"))
 EXTERNAL_STORAGE = "~/external-storage/"
 
-
+#Function to verify if a new storage connected to the Pi
 def isAvailableDrive():
 	#Read the storages connected to the RB
 	storages = os.popen("sudo blkid").readlines()
@@ -14,38 +13,39 @@ def isAvailableDrive():
 		#return the external drive's directory (sdx)
 		return drive
 	else:
-		#print("\tNo Storage")
 		return None
 
 #Function to mount the external drive, copy the Rom files and umount the external drive
 def getFiles(drive, actualRoms):
 	newRoms = []
-	#print("Storage mounted: " + drive + " " + EXTERNAL_STORAGE)
 	os.system("sudo mount " + drive + " " + EXTERNAL_STORAGE)
-	data = os.popen("ls " + EXTERNAL_STORAGE + "ROMS/").readlines()
-	#print("Read: ", data)
-	for rom in data:
-		rom = rom.split("\n")[0]
-		if ".SMC" in rom and rom not in actualRoms:
-			newRoms.append(rom)
-			os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.SMC ~/ROMS/")
-		elif ".smc" in rom and rom not in actualRoms:
-			newRoms.append(rom)
-			os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.smc ~/ROMS/")
-		elif ".sfc" in rom and rom not in actualRoms:
-			newRoms.append(rom)
-			os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.sfc ~/ROMS/")
-		elif ".SFC" in rom and rom not in actualRoms:
-			newRoms.append(rom)
-			os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.SFC ~/ROMS/")
-		elif ".png" in rom:
-			os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.png ~/ROMS/")
-	#print("Files copied. ", newRoms, " added to the local ROMS")
-	time.sleep(1)
-	os.system("sudo umount " + EXTERNAL_STORAGE)
-	os.system("sudo eject " + drive)
-	#print("umounted storage. Remove the storage")
-	while(isAvailableDrive()):
-		os.system("sudo eject " + drive)
-		time.sleep(10)
+	storage = os.popen("ls " + EXTERNAL_STORAGE).readlines()
+	#Search for a "ROMS" directory. If doesn't exist, the script ends. If exist, copy the ROMs files
+	if "ROMS\n" in storage:
+		data = os.popen("ls " + EXTERNAL_STORAGE + "ROMS/").readlines()
+		for rom in data:
+			rom = rom.split("\n")[0]
+			if ".SMC" in rom and rom not in actualRoms:
+				newRoms.append(rom)
+				os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.SMC ~/ROMS/")
+			elif ".smc" in rom and rom not in actualRoms:
+				newRoms.append(rom)
+				os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.smc ~/ROMS/")
+			elif ".sfc" in rom and rom not in actualRoms:
+				newRoms.append(rom)
+				os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.sfc ~/ROMS/")
+			elif ".SFC" in rom and rom not in actualRoms:
+				newRoms.append(rom)
+				os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.SFC ~/ROMS/")
+			elif ".png" in rom:
+				os.system("sudo cp " + EXTERNAL_STORAGE + "ROMS/*.png ~/ROMS/")
+		while(isAvailableDrive()):
+			os.system("sudo umount " + EXTERNAL_STORAGE)
+			os.system("sudo eject " + drive)
+			time.sleep(3)
+	else:
+		newRoms.append("NoROM")
 	return newRoms
+
+#if isAvailableDrive():
+#	getFiles("/dev/sda1", [])
